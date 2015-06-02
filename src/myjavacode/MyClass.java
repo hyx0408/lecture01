4a2dbb530a5a239772d164d79d0f0def259174eb
HubertHuang <hyx0408@126.com>
2015-06-02 14:54:41 +0800

    Initial commit

---
+++ src/myjavacode/MyClass.java
@@ -0,0 +1,22 @@
+package myjavacode;
+import java.util.Arrays;
+
+/**
+ * Created by hubert on 2015.06.01.
+ */
+public class MyClass {
+    public static void main(String [] args){
+        String saySomething = "Hello Hubert Huang";
+        String[] sayArray = {"Hello","Hubert","Huang"};
+        if(sayArray.length == 3) {
+            System.out.println(saySomething);
+            System.out.println(saySomething.length());
+            double d =Math.sin(Math.PI/2);
+            System.out.println(d);
+            String[] saySamething = Arrays.copyOf(sayArray,sayArray.length);
+            for(String word : sayArray)
+                System.out.print(word+' ');
+            System.out.println();
+        }
+    }
+}
